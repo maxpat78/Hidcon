17146e9ec087567b3f1d0340b434cdf8a01ba923
maxpat78 <maxpat78@yahoo.it>
2021-04-20 08:08:45 +0200

    Update hidcon.c
    ShellExecute command line is now correctly built

--- hidcon.c
+++ hidcon.c
@@ -1,9 +1,9 @@
-// Executes from command line hiding the console window
+// Execute console apps (with ShellExecute "open") hiding the console window
 #define UNICODE
 
 #include <windows.h>
 
-#pragma comment(linker,"/ENTRY:_Go_")
+#pragma comment(linker,"/ENTRY:_Go_") // custom entry point
 
 #pragma comment(linker,"/SUBSYSTEM:Windows")
 
@@ -11,29 +11,30 @@
 #pragma comment(linker,"/DEFAULTLIB:USER32.lib")
 #pragma comment(linker,"/DEFAULTLIB:KERNEL32.lib")
 
-void _Go_()
-{
+void _Go_() {
     LPWSTR* Argv;
     LPWSTR Args;
     int Argc, i;
+    //wchar_t s[100];
 
     Argv = CommandLineToArgvW(GetCommandLineW(), &Argc);
 
-    if (Argc < 2)
-        goto Exit;
+    if (Argc < 2) goto Exit;
 
     Args = LocalAlloc(LMEM_FIXED|LMEM_ZEROINIT, Argc + 2*lstrlen(GetCommandLineW()));
     
-    if (Argc > 2)
-    {
-        for (i=2; i < Argc; i++)
-        {
+    if (Argc > 1) {
+        for (i=1; i < Argc; i++) {
              lstrcat(Args, Argv[i]);
              lstrcat(Args, L" ");
         }
     }
     //MessageBox(0, Args, L"Debug", MB_OK);
-    ShellExecute(0, L"open", Argv[1], Args, 0, 0);
+    i = ShellExecute(0, L"open", Argv[1], Args, 0, 0);
+    if (i < 33)
+        MessageBox(0, L"Hidcon failed in launching hidden console process!", L"Hidcon", MB_OK);
+    //wsprintf(s, L"ERR=0x%08X", i);
+    //MessageBox(0, s, L"Debug", MB_OK);
 Exit:
-    ExitProcess(0);
+    ExitProcess(i);
 }
